1c25e209cc4a6a61207702f5306e12afcda1e9af
Garrett Grimm <garrett@grimmdude.com>
2021-04-07 21:23:02 -0700

    2.0.14

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@
  * Constants used in player.
  */
 const Constants = {
-	VERSION: '2.0.13',
+	VERSION: '2.0.14',
 	NOTES: [],
 	HEADER_CHUNK_LENGTH: 14,
 	CIRCLE_OF_FOURTHS: ['C', 'F', 'Bb', 'Eb', 'Ab', 'Db', 'Gb', 'Cb', 'Fb', 'Bbb', 'Ebb', 'Abb'],
